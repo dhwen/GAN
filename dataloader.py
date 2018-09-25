05c081b9db66eec28396af6c0a7b0d7b263f8c31
dhwen <daniel.h.wen@gmail.com>
2018-09-24 17:09:21 -0700

    Created a json reader to load configurable model/training/debug settings

--- dataloader.py
+++ dataloader.py
@@ -6,23 +6,23 @@ class DataLoader:
     def __init__(self):
         pass
 
-    def generate_true_samples(self, type, sample_count, dim):
+    def generate_true_samples(self, count, dims, type):
 
         if type == "numerical":
-            true_samples = np.zeros((sample_count,dim))
-            for i in range(sample_count):
-                true_samples[i] = np.random.randint(100,105,dim) #True samples are randomly generated values from 100 to 105
+            true_samples = np.zeros((count,dims[0]))
+            for i in range(count):
+                true_samples[i] = np.random.randint(100,105,dims[0]) #True samples are randomly generated values from 100 to 105
             return true_samples
         elif type =="mnist":
             data = input_data.read_data_sets("MNIST_data/", one_hot=True)
-            true_samples = np.asarray(data.train.next_batch(sample_count)[0])
-            np.reshape(true_samples,(sample_count,28,28))
+            true_samples = np.asarray(data.train.next_batch(count)[0])
+            np.reshape(true_samples,(count,28,28))
             return true_samples
         else:
             print("Unsupported type")
 
-    def generate_noise_samples(self, sample_count, dim=10, mu=0, sigma=1):
-        noise_samples = np.zeros((sample_count,dim))
-        for i in range(sample_count):
-            noise_samples[i]= np.random.normal(mu,sigma,dim) #Seed noise for the generative network
+    def generate_noise_samples(self, count, dims, mu=0, sigma=1):
+        noise_samples = np.zeros((count,dims[0]))
+        for i in range(count):
+            noise_samples[i]= np.random.normal(mu,sigma,dims[0]) #Seed noise for the generative network
         return noise_samples
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import tensorflow as tf
 import numpy as np
 import os
+import json
 from GAN import GAN
 from dataloader import DataLoader
+from config_utils import ConfigLoader
 
-ckpt_path = 'model_ckpt/'
-if not os.path.isdir(ckpt_path):
-    os.makedirs(ckpt_path)
+file_json = "config_numerical.json"
+config = ConfigLoader(file_json)
+
+if not os.path.isdir(config.ckpt_save_path):
+    os.makedirs(config.ckpt_save_path)
 
 loader = DataLoader()
-true_samples = loader.generate_true_samples(type="numerical", sample_count=400, dim=1)
-noise_samples = loader.generate_noise_samples(sample_count=400, dim=10)
+noise_samples = loader.generate_noise_samples(count=config.noise_count, dims=config.noise_dims)
+true_samples = loader.generate_true_samples(count=config.samples_count, dims=config.samples_dims, type=config.samples_type)
 
-net = GAN(input_dims=[10], output_dims=[1], type="DNN")
-
-num_train_epochs_outter = 40
-num_train_epochs_gen = 5000
-num_train_epochs_disc = 5000
+net = GAN(input_dims=config.noise_dims, output_dims=config.samples_dims, type=config.model_type)
 
 with tf.Session(graph=net.graph) as sess:
     sess.run(tf.global_variables_initializer())
@@ -35,7 +35,7 @@ with tf.Session(graph=net.graph) as sess:
 
     data_labels = np.zeros((len(noise_samples)+len(true_samples), 2))
 
-    for i in range(num_train_epochs_outter):
+    for i in range(config.num_epochs_main):
 
         false_samples = sess.run(net.output_gen, feed_dict={net.input_gen: noise_samples, net.label: noise_labels})
 
@@ -47,16 +47,16 @@ with tf.Session(graph=net.graph) as sess:
         data = data[idcs_new - 1]
         data_labels = data_labels[idcs_new - 1]
 
-        for j in range(num_train_epochs_disc):
+        for j in range(config.num_epochs_disc):
             [opt, output, loss] = sess.run([net.opt_disc, net.output_disc, net.loss], feed_dict={net.input_disc: data, net.label: data_labels})
-            if j % 99 == 0 :
-                print('Epoch %d %d, training loss for the discriminative network is %g' % (i, j, loss))
+            if (j+1) % config.print_interval_loss_disc == 0 :
+                print('Epoch %d %d, training loss for the discriminative network is %g' % (i+1, j+1, loss))
 
-        for j in range(num_train_epochs_gen):
+        for j in range(config.num_epochs_gen):
             [opt, output, loss] = sess.run([net.opt_gen, net.output_disc, net.loss], feed_dict={net.input_gen: noise_samples, net.label: noise_labels})
-            if j % 99 == 0 :
-                print('Epoch %d %d, training loss for the generative network is %g' % (i, j, loss))
+            if (j+1) % config.print_interval_loss_gen == 0 :
+                print('Epoch %d %d, training loss for the generative network is %g' % (i+1, j+1, loss))
 
-        if i % 9 == 0:
+        if (i+1) % config.save_interval_ckpt == 0:
             print(false_samples)
-            saver.save(sess, ckpt_path + "GAN.ckpt")
+            saver.save(sess, config.ckpt_save_path + config.ckpt_file_name)
